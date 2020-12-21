25314c365b524303b1ffec883d69efe549e12490
Syed Daniyal Ali <SyedDaniyalAli20@gmail.com>
2020-12-21 23:22:23 +0500

    added return type in function

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -10,17 +10,16 @@ class Chart extends StatelessWidget {
   Chart(this.recentTransaction);
 
   List<Map<String, Object>> get groupedTransactionValues {
-    List.generate(7, (index) {
+    return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(Duration(days: index),);
       var totalSum = 0.0;
 
       for (var tx in recentTransaction) {
         if (tx.date.day == weekDay.day &&
             tx.date.month == weekDay.month &&
-            tx.date.year == weekDay.year)
-          {
-            totalSum+=tx.amount;
-          }
+            tx.date.year == weekDay.year) {
+          totalSum += tx.amount;
+        }
       }
 
       return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
